cd39877ee976802191ed6767e7733aec9f1003e9
Santiago Guastavino <san.guasta@gmail.com>
2023-06-05 13:59:27 -0300

    updated nvm to match render

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -67,7 +67,7 @@ export class EmailService {
     });
 
     const mailOptions = {
-      from: `Selfieboost<${process.env.MAIL_FROM}>`,
+      from: `Notenook<${process.env.MAIL_FROM}>`,
       to: this.recipient.toLowerCase(),
       subject: this.emailData.subject,
       html: compiledTemplate(this.emailData),
